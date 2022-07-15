d6ef9c2879f23964ff06d07abd1fd429b0c0e467
abidikhairi <abidikhairi.1337@gmail.com>
2022-07-15 13:33:59 +0200

    load citation datasets

--- citation_datasets.py
+++ citation_datasets.py
@@ -1,8 +1,29 @@
+import torch as th
 import dgl.data as datasets
 
 
 def main():
-    pass
+    cora = datasets.CoraGraphDataset(reverse_edge=False)
+    graph = cora[0]
+
+    adj = graph.adj().to_dense()
+    clusters = graph.ndata['label'].squeeze()
+    nfeats = graph.ndata['feat']
+    
+    th.save(adj, 'data/cora/adj.pt')
+    th.save(clusters, 'data/cora/clusters.pt')
+    th.save(nfeats, 'data/cora/nfeats.pt')
+
+    citeseer = datasets.CiteseerGraphDataset(reverse_edge=False)
+    graph = citeseer[0]
+
+    adj = graph.adj().to_dense()
+    clusters = graph.ndata['label'].squeeze()
+    nfeats = graph.ndata['feat']
+
+    th.save(adj, 'data/citeseer/adj.pt')
+    th.save(clusters, 'data/citeseer/clusters.pt')
+    th.save(nfeats, 'data/citeseer/nfeats.pt')
 
 
 if __name__ == '__main__':
