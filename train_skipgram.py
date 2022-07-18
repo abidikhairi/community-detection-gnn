d1d7ec65c7703a16d00c6addc2abaf8147fe4aed
abidikhairi <abidikhairi.1337@gmail.com>
2022-07-18 13:59:17 +0200

    train node2vec model

--- train_skipgram.py
+++ train_skipgram.py
@@ -1,7 +1,7 @@
 import argparse
 from project.utils.utils import adjacency_to_nxg
 from project.models.deepwalk import DeepWalk
-
+from project.models.n2v import Node2VecTrainer
 
 def main(args):
     adj_file = args.adj
@@ -18,8 +18,10 @@ def main(args):
 
     if model == 'deepwalk':
         model = DeepWalk(graph, num_walks, walk_length, vector_size, epochs, window_size, negatives)
-    else :
-        raise ValueError('model {} not implemented yet !!'.format(model))
+    elif model == 'node2vec':
+        model = Node2VecTrainer(graph, num_walks, walk_length, vector_size, epochs, window_size, negatives)
+    else:
+        raise ValueError(f'Unknown model {model}, available models are deepwalk & node2vec')
 
     model.train()
     model.save_embeddings(output)
@@ -32,6 +34,8 @@ if __name__ == '__main__':
     parser.add_argument('--model', type=str, required=True, choices=['deepwalk', 'node2vec'], help='Model to train (Deepwalk or Node2Vec)')
     parser.add_argument('--num-walks', type=int, default=10, help='Number of random walks to use. Defaults: 10')
     parser.add_argument('--walk-length', type=int, default=10, help='Length of each random walk. Defaults: 10')
+    parser.add_argument('--p', type=float, default=1.0, help='Return probability for node2vec. Defaults: 1.0')
+    parser.add_argument('--q', type=float, default=1.0, help='Exploring probability for node2vec. Defaults: 1.0')
     parser.add_argument('--vector-size', type=int, default=128, help='Dimension of the embedding vectors. Defaults: 128')
     parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train. Defaults: 10')
     parser.add_argument('--window-size', type=int, default=10, help='Window size for skipgram. Defaults: 10')
