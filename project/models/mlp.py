5fa46557b4d360c48328508adc05dd1788360afc
abidikhairi <abidikhairi.1337@gmail.com>
2022-07-18 15:03:40 +0200

    add linear projection module

---
+++ project/models/mlp.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+
+class Perceptron(nn.Module):
+    def __init__(self, nfeats, ncomms) -> None:
+        super().__init__()
+
+        self.linear = nn.Linear(nfeats, ncomms, bias=False)
+
+    def forward(self, x):
+        return self.linear(x)
